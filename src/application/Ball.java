da996970dd8acfd6bb7c94a8c7ed274e46b64f95
Anna Rakes <arakes@elon.edu>
2025-10-05 17:30:46 -0400

    Ball, Brick, and BrickWall Classes Made

--- src/application/Ball.java
+++ src/application/Ball.java
@@ -1,12 +1,80 @@
+import java.util.Random;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class Ball {
 	
-	private double xCoordinate;
-	private double yCoordinate;
-	private double dx;
-	private double dy;
-	private double radius;
-	private static final int RANDOMNESS = -1;
+	private final double radius = 10;
+    public final int BALL_MIN_SPEED = -400;
+    public final int BALL_MAX_SPEED = 400;
+	
+	private Circle ball;
+    private Point2D ballVelocity;
+    private Random random = new Random();
+    private boolean gameLost;
+    
+	
+	public void createBall(int windowWidth, int windowHeight) {
+		ball = new Circle();		
+		double xCoordinate = windowWidth/2;
+		double yCoordinate = windowHeight*2.0/3.0;
+		
+		ball.setCenterX(xCoordinate);
+		ball.setCenterY(yCoordinate);
+		ball.setRadius(radius);
+		ball.setFill(Color.BLACK);
+		
+		ballVelocity = new Point2D(getRandomInRange(BALL_MIN_SPEED, BALL_MAX_SPEED),
+                getRandomInRange(BALL_MIN_SPEED, BALL_MAX_SPEED));
+	}
+	
+	/**
+     * Move by taking one step based on its velocity.
+     *
+     * Note, elapsedTime is used to ensure consistent speed across different machines.
+     */
+    public void move (double elapsedTime) {
+        ball.setCenterX(ball.getCenterX() + ballVelocity.getX() * elapsedTime);
+        ball.setCenterY(ball.getCenterY() + ballVelocity.getY() * elapsedTime);
+    }
+
+    /**
+     * Bounce off the walls represented by the edges of the screen.
+     */
+    public boolean keepBallWithinBounds (double screenWidth, double screenHeight) {
+        // collide all bouncers against the walls
+        if (ball.getCenterX() < 0 || ball.getCenterX() > screenWidth - ball.getBoundsInLocal().getWidth()) {
+            ballVelocity = new Point2D(-ballVelocity.getX(), ballVelocity.getY());
+        }
+        if (ball.getCenterY() < 0 ) {
+            ballVelocity = new Point2D(ballVelocity.getX(), -ballVelocity.getY());
+        }
+        if (ball.getCenterY() > screenHeight - ball.getBoundsInLocal().getHeight()) {
+        		return true;
+        }
+        return false;
+    }
+    
+    // Returns an "interesting", non-zero random value in the range (min, max)
+    private int getRandomInRange (int min, int max) {
+        return min + random.nextInt(max - min) + 1;
+    }
+	
+	public Circle getBall() {
+		return ball;
+	}
+	
+	public boolean checkIfGameLost() {
+		if (gameLost) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 
 
 
---
+++ src/application/BrickWall.java
@@ -0,0 +1,55 @@
+// Anna Rakes
+// Store collection of bricks and position them into a wall.
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+
+public class BrickWall {
+	
+	private ArrayList<Rectangle> brickWall = new ArrayList<>();
+    private Random random = new Random();
+	private Paint[] colorList = {Color.LIGHTCORAL, Color.LAVENDERBLUSH, Color.LIGHTSTEELBLUE, Color.MOCCASIN, 
+			Color.LIGHTGOLDENRODYELLOW,Color.DARKSEAGREEN};
+	private int windowWidth;
+	private int windowHeight;
+	private int numberOfColumns;
+	private int numberOfRows;
+	private Brick emptyBrick = new Brick();
+
+	
+	public BrickWall(int windowWidth, int windowHeight) {
+		this.windowHeight = windowHeight;
+		this.windowWidth = windowWidth;
+		numberOfColumns = this.windowWidth/ emptyBrick.getBrickWidth();
+		numberOfRows = (this.windowHeight/3) / emptyBrick.getBrickHeight();
+	}
+
+	
+	public void gatherAllBricks() {
+		for (int i = 0; i<numberOfRows ; i++) {
+			for (int j = 0; j<numberOfColumns; j++) {
+				double xCoordinate = j*emptyBrick.getBrickWidth();
+				double yCoordinate = i*emptyBrick.getBrickHeight();
+				Brick brick = new Brick(generateRandomBrickColor(), xCoordinate, yCoordinate);
+				brick.createBrick();
+				brickWall.add(brick.getBrick());
+			}
+		}
+	}
+	
+	public Paint generateRandomBrickColor () {
+		Paint brickColor = colorList[random.nextInt(6)];
+		return brickColor;
+	}
+	
+	public ArrayList<Rectangle> getBrickWall () {
+		return brickWall;
+	}
+
+}
