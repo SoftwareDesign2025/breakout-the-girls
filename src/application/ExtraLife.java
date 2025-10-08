996f2908a53d543a1218f4486a8283bab3681ecf
Katherine Hoadley <khoadley@elon.edu>
2025-10-08 11:37:17 -0400

    fixed abstract override

--- src/application/ExtraLife.java
+++ src/application/ExtraLife.java
@@ -2,18 +2,19 @@
 package application;
 import javafx.scene.paint.Color;
 
-public class ExtraLife extends PowerUp{
+public abstract class ExtraLife extends PowerUp{
 
 	public ExtraLife(double x, double y) {
         super(x, y, Color.LIGHTGREEN); //light green visual to represent life
     }
 	
 	 @Override
-	    public void activatePowerUp(Game game) {
-	        // Add one life to the player
-	        game.setLives(game.getLives() + 1);
+	 public int activatePowerUp(int lives) {
+	    // Add one life to the player
+	    lives++;
 
-	        // Deactivate so it stops moving and disappears
-	        setActive(false);
-	    }
+	    // Deactivate so it stops moving and disappears
+	    setActive(false);
+	    return lives;
+	}
 }
